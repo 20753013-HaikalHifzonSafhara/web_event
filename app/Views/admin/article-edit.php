4fc6c04d5c66b1849e8adeb2d9e3273c0e6067c2
Haikal Hifzon Safhara <86160950+20753013-HaikalHifzonSafhara@users.noreply.github.com>
2022-12-10 11:47:31 +0700

    Update article-edit.php
    ubah lorem ipsum => Edit data artikel yang ingin di rubah
    ubah Ummernote => Artikel Editor

--- app/Views/admin/article-edit.php
+++ app/Views/admin/article-edit.php
@@ -6,14 +6,14 @@
         <div class="row">
             <div class="col-12 col-md-6 order-md-1 order-last">
                 <h3>Artikel Editor </h3>
-                <p class="text-subtitle text-muted">Lorem ipsum dolor sit amet
+                <p class="text-subtitle text-muted">Edit data artikel yang ingin di rubah
                 </p>
             </div>
             <div class="col-12 col-md-6 order-md-2 order-first">
                 <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Summernote</li>
+                        <li class="breadcrumb-item active" aria-current="page">Artikel Editor</li>
                     </ol>
                 </nav>
             </div>
